7abbcb2e990d39ded51c253573b4a97f8daa92c4
Binag <ricardo.athayde.1@gmail.com>
2020-06-23 19:32:09 -0300

    First Commit

---
+++ Assets/Scripts/JsonReader.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON; //Using a common JSON parser since Unity internal JsonUtility does not support nested arrays.
+using System.IO;
+
+public class JsonReader : MonoBehaviour
+{
+    //Used to let the other scripts know we just updated the JSON
+    public delegate void LoadedNewJSON();
+    public LoadedNewJSON loadedNewJSON;
+
+
+    string filePath; //Path to the JSON File
+    public string title { get; private set; } //The title inside the JSON File
+    public string[,] matrix { get; private set; } //The matrix with all the json columns and rows inside the Data
+
+    DateTime lastFileModify; //The time we last updated the JSON File. Used to check if we need to reload the file because it was modified 
+
+    public static JsonReader self; //Singleton       
+    private void Awake()
+    {
+        //Creeates the singleton in a safe way
+        if(self == null)
+        {
+            self = this;
+        }
+        else
+        {
+            Debug.LogError("Duplicate singleton found. Deleting new one.");
+            DestroyImmediate(this);
+        }
+    }
+
+    void Start()
+    {
+        filePath = Path.Combine(Application.streamingAssetsPath, "JsonChallenge.json");
+    }
+
+    private void Update()
+    {
+        if(File.GetLastWriteTime(filePath) > lastFileModify) //Check if the file was modified and reads de JSON for the first time
+        {            
+            ReadJson();
+        }
+    }
+
+    public void ReadJson()
+    {
+        lastFileModify = File.GetLastWriteTime(filePath); //Save the last modification time of the file to make sure we are reading the file when it is modified
+
+        bool fileLoaded = false;
+        StreamReader reader = null;
+        string testJson = "";
+        while (!fileLoaded) //We keep trying to read the JSON file. This is important because the file is modified before it finishes saving and the other program can still be accessing it and it will return a error. 
+        { 
+            try
+            {
+                reader = new StreamReader(filePath);
+                testJson = reader.ReadToEnd();
+                reader.Close();
+                fileLoaded = true;
+            }
+            catch
+            {
+                fileLoaded = false;
+            }
+        }
+
+        var json = JSON.Parse(testJson);
+        title = json["Title"].Value;
+        matrix = new string[json["ColumnHeaders"].Count, json["Data"].Count + 1]; //Creating our matrix array
+
+        //Placing the headers acording to the ColumnHeaders
+        for (int i = 0; i < json["ColumnHeaders"].Count; i++) 
+        {
+            matrix[i, 0] = json["ColumnHeaders"][i].Value;
+        }
+
+        //Placing the rows acording to the Data
+        for (int k = 0; k < matrix.GetLength(1)-1; k++)//Rows
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)//Columns
+            {
+                matrix[i, k+1] = json["Data"][k][matrix[i, 0]].Value;             
+            }
+        }
+        loadedNewJSON?.Invoke(); //Tell all other scripts that we updated the matrix
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] GameObject TextPrefab = default; //Using a prefab as a base for the texts
+    [SerializeField] Text title = default; //Title Text
+
+    [Range(0,200)]
+    [Tooltip("Default Font size for the cells")]
+    [SerializeField] int textSize = default; //Default Font size
+    [Range(0, 2)]
+    [Tooltip("Multily the default font size for the headers")]
+    [SerializeField] float headerSizeMultiplyer = default; //Multily the default font size for this
+
+    [SerializeField] GridLayoutGroup layout = default; //The layout gropu used to organize the columns and rows
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JsonReader.self.loadedNewJSON += UpdateUI; //Subscribe to be notied when ther eis an update on the JSON
+    }
+
+    //Updates the UI
+    void UpdateUI()
+    {
+        foreach(Transform obj in layout.transform) //Find all previous objects and destroy them before creating a new grid
+        {
+            Destroy(obj.gameObject);
+        }
+
+        title.text = JsonReader.self.title;
+        layout.constraintCount = JsonReader.self.matrix.GetLength(0); //The grid layout is updated to have the correct number of columns
+        for (int k = 0; k < JsonReader.self.matrix.GetLength(1); k++)
+        {
+            for (int i = 0; i < JsonReader.self.matrix.GetLength(0); i++)
+            {
+                Text txt = Instantiate(TextPrefab, layout.transform).GetComponent<Text>(); //Creates the text object
+                txt.text = JsonReader.self.matrix[i, k];
+                txt.fontSize = k != 0 ? textSize : (int)(textSize * headerSizeMultiplyer);
+                txt.fontStyle = k != 0 ? FontStyle.Normal : FontStyle.Bold;
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        if(JsonReader.self)
+        {
+            JsonReader.self.loadedNewJSON += UpdateUI;  //Subscribe to be notied when ther eis an update on the JSON
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (JsonReader.self)
+        {
+            JsonReader.self.loadedNewJSON -= UpdateUI; //Unsubscribe to be notied when ther eis an update on the JSON
+        }
+    }
+}
